dcd152fb29ca7cf13eff2aea787fdbc3eeddeb0c
diegoreto7u7 <diegoret2005@gmail.com>
2024-11-05 10:08:42 +0100

    xvxc

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  photos: string[] = [
+    'assets/img/almi.jpeg',
+    'assets/img/almi.jpeg',
+    'assets/img/almi.jpeg',
+    'assets/img/almi.jpeg',
   
-  constructor() {}
+  ];
 
+  constructor() {}
 }
