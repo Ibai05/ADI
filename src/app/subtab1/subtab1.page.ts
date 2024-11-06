17410a22ec1cfd0b37a6a6a2a02cb7dc5f64468d
diegoreto7u7 <diegoret2005@gmail.com>
2024-11-06 09:56:33 +0100

    fjfjhfh

--- src/app/subtab1/subtab1.page.ts
+++ src/app/subtab1/subtab1.page.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subtab1.page.scss'],
 })
 export class Subtab1Page implements OnInit {
+  productos = [
+    { nombre: 'Resident Eil', precio: 29.99, imagen: 'assets/img/residentevil.jpg' },
+    { nombre: 'Producto 2', precio: 49.99, imagen: 'assets/img/residentevil.jpg' },
+    { nombre: 'Producto 3', precio: 15.99, imagen: 'assets/img/residentevil.jpg' },
+    { nombre: 'Producto 4', precio: 99.99, imagen: 'assets/img/residentevil.jpg' },
+    { nombre: 'Producto 5', precio: 79.99, imagen: 'assets/img/residentevil.jpg' } 
+  ];
 
   constructor() { }
 
